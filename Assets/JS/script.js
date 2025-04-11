b393e06ad8122f2fb65927f054cfbfc2fd2c1786
Victor-Queiroz-Mendes <victor.h.mendes7@aluno.senai.br>
2025-04-11 12:28:30 -0300

    atualizando

--- Assets/JS/script.js
+++
@@ -1,8 +0,0 @@
-// Exemplo de interatividade com JS: alerta ao clicar em “Ver detalhes”
-document.querySelectorAll('.btn').forEach(btn => {
-    btn.addEventListener('click', function (e) {
-      e.preventDefault();
-      alert('Em breve você verá os detalhes deste carro de luxo!');
-    });
-  });
-  
